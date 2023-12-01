cfc4a9b2c0e762120c5cb04561761379c3ab9cfb
Lactor <lactorjm4@gmail.com>
2023-12-01 11:03:37 +0300

    Assignement

--- server/app.py
+++ server/app.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response
+# app.py
+from flask import Flask, render_template
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
@@ -13,22 +11,59 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+def get_animal_info(animal_id):
+    if animal_id == 1:
+        return Animal(name="Lion", species="Panthera leo")
+    else:
+        return None
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = get_animal_info(id)
+    return render_template('animal.html', animal=animal)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    return render_template('enclosure.html', enclosure=enclosure)
 
+@app.route('/animal-route/<int:id>')
+def animal_route(id):
+    animal = get_animal_info(id)
+
+    if animal:
+        return f'''
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Animal Info</title>
+            </head>
+            <body>
+                <ul>
+                    <li>Name: {animal.name}</li>
+                    <li>Species: {animal.species}</li>
+                </ul>
+            </body>
+            </html>
+        '''
+    else:
+        return "Animal not found", 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
